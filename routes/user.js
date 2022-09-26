cbb63b23ff8fa5d53b9ba0087a54e6b1d3bd1ad0
sfiwmoc <sfiwmoc@gmail.com>
2022-09-26 12:47:14 +0530

    login and signup front end

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,17 @@ router.get('/', function(req, res, next) {
 
     res.render('user/view-workers',{admin:false,workers})
   })
- 
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('user/login',{admin:false})
+})
+
+router.get('/signup', function(req, res, next) {
+  res.render('user/signup',{admin:false})
+})
+
+router.post('/signup', function(req, res, next) {
+  
+})
 module.exports = router;
